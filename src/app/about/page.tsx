118554a9cc600e399e71d25dd4d9607eface15af
kevin.chou <kevin.chou@wabow.com>
2025-10-02 10:12:23 +0800

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+        <div className="px-4 max-w-4xl mx-auto">
+            About page
+        </div>
+    )
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,7 +7,8 @@ export const navItems = [
     {name: 'Home', path: '/'},
     {name: 'Work', path: '/work'},
     {name: 'Projects', path: '/projects'},
-    {name: 'Blog', path: '/blog'}
+    {name: 'Blog', path: '/blog'},
+    {name: 'About', path: '/about'}
 ]
 
 /**
