461ed7eb597afb00436ee97de1e69eb786060ac7
shanyueel <wulingkevin0704@gmail.com>
2025-10-04 08:37:22 +0800

    remove alpha camp from work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
                 viewport={{once: true}}
                 className="mt-16"
             >
-                <ViewAllHeader title="Work & Experience" pageUrl="/work" itemCount={work.length}/>
+                <ViewAllHeader title="Work Experience" pageUrl="/work" itemCount={work.length}/>
                 <div className="grid gap-4">
                     {work.slice(0, 3).map((job, i) => (
                         <motion.div
