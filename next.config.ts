82933f811ee50533abab29782d13292737ad6ccd
kevin.chou <kevin.chou@wabow.com>
2025-10-27 09:45:29 +0800

    install svgr for svg file usage

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next"
+import type { RuleSetRule } from "webpack"
 import createMDX from "@next/mdx"
 import remarkGfm from "remark-gfm"
+import { RuleSet } from "natural"
 
 // Create MDX configuration
 const withMDX = createMDX({
@@ -15,6 +17,30 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  webpack: config => {
+    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule) =>
+      rule.test?.test?.(".svg")
+    )
+    if (fileLoaderRule) fileLoaderRule.exclude = /\.svg$/
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: true,
+            svgoConfig: {
+              plugins: [{ name: "removeViewBox", active: false }],
+            },
+            titleProp: true,
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
 
 // Merge MDX config with Next.js config
