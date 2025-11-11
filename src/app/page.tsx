06ed43543f4f9378ad8c8ec3a282c9635c8040d3
kevin.chou <kevin.chou@wabow.com>
2025-11-11 13:43:17 +0800

    feat: adjust homePage Breif Intro Area and details.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,23 @@
 import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
 import type { IconType } from "react-icons"
-import {
-  FaMapMarkerAlt,
-  FaLanguage,
-  FaUniversity,
-  FaBuilding,
-  FaPaintBrush,
-  FaGithub,
-  FaLinkedin,
-  FaEnvelope,
-} from "react-icons/fa"
+import { FaCode, FaPaintBrush, FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"
 import { HiDownload } from "react-icons/hi"
 import { AiFillMessage } from "react-icons/ai"
+import { MdShoppingCart } from "react-icons/md"
+import { RiLoopRightLine } from "react-icons/ri"
 import Lottie from "lottie-react"
 import { motion, AnimatePresence } from "framer-motion"
-import { techStackMap } from "@/lib/constants"
-import { TechIconColors } from "@/lib/types"
-import blog from "@/data/blog"
-import projects from "@/data/projects"
-import work from "@/data/work"
-import Button from "@/components/ui/Button"
-import WorkItem from "@/components/features/WorkItem"
+import ViewAllHeader from "@/components/layout/ViewAllHeader"
 import ProjectCard from "@/components/features/ProjectCard"
 import BlogPost from "@/components/features/BlogPost"
-import ViewAllHeader from "@/components/layout/ViewAllHeader"
+import Button from "@/components/ui/Button"
+import Tag from "@/components/ui/Tag"
+import { Timeline, TimelineItem } from "@/components/mdx/Timeline"
 import Logo from "@/assets/icons/logo.svg"
 import scrollDownArrows from "@/assets/animations/scrollDownArrows.json"
+import blog from "@/data/blog"
+import projects from "@/data/projects"
 
 interface Fact {
   icon: IconType
@@ -41,6 +32,7 @@ interface Fact {
  */
 export default function Home() {
   const ProjectsRef = useRef<HTMLDivElement>(null)
+  const BriefIntroRef = useRef<HTMLDivElement>(null)
 
   const getTimeSafe = (dateStr: string | undefined) => {
     const date = new Date(dateStr ?? "")
@@ -52,26 +44,15 @@ export default function Home() {
     "detail-oriented",
     "collaborative",
     "growth-minded",
+    "challenge-seeker",
   ]
   const [charIndex, setCharIndex] = useState(0)
 
   const facts: Fact[] = [
-    { icon: FaCode, label: "Frontend Engineer" },
-    { icon: FaPaintBrush, label: "Design Background" },
-    { icon: FaBuilding, label: "Wabow Information Inc." },
-    { icon: FaUniversity, label: "CS Student @ Oregon State Univ." },
-    { icon: FaLanguage, label: "ZH-Native | EN-Fluent" },
-    { icon: FaMapMarkerAlt, label: "Taipei, Taiwan" },
-  ]
-
-  const techStack: string[] = [
-    "React",
-    "Vue",
-    "TypeScript",
-    "Python",
-    "TailwindCSS",
-    "Git",
-    "Firebase",
+    { icon: MdShoppingCart, label: "E-commerce" },
+    { icon: FaPaintBrush, label: "Design Mindset" },
+    { icon: FaCode, label: "CS Fundamentals" },
+    { icon: RiLoopRightLine, label: "Agile Development" },
   ]
 
   const scrollToArea = (TargetRef: React.RefObject<HTMLDivElement>) => {
@@ -81,7 +62,7 @@ export default function Home() {
   useEffect(() => {
     const charInterval = setInterval(() => {
       setCharIndex(i => (i + 1) % characteristics.length)
-    }, 2000)
+    }, 2500)
     return () => clearInterval(charInterval)
   })
 
@@ -124,14 +105,20 @@ export default function Home() {
           </div>
 
           <div className="flex gap-2 mt-2 md:gap-4">
-            <Button color="primary" className="flex-1 flex items-center gap-1">
+            <Button
+              color="primary"
+              className="flex-1 flex items-center gap-1"
+              onClick={() => scrollToArea(BriefIntroRef)}
+            >
               <AiFillMessage size={16} />
-              Contact Me
+              Who am I ?
             </Button>
-            <Button color="secondary" className="flex-1 flex items-center gap-1">
-              <HiDownload size={16} />
-              Download CV
-            </Button>
+            <Link href="#">
+              <Button color="secondary" className="flex-1 flex items-center gap-1">
+                <HiDownload size={16} />
+                Download CV
+              </Button>
+            </Link>
           </div>
 
           <div className="flex justify-center gap-6 mt-4 text-xl">
@@ -175,7 +162,7 @@ export default function Home() {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
         viewport={{ once: true, amount: 0.3 }}
-        className="main-container h-full-container mt-12 scroll-mt-header"
+        className="main-container mt-12 scroll-mt-header"
       >
         <ViewAllHeader title="Recent Projects" pageUrl="/projects" itemCount={projects.length} />
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
@@ -192,88 +179,6 @@ export default function Home() {
               />
             )
           })}
-        </div>
-      </motion.div>
-
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-        viewport={{ once: true }}
-        className="main-container h-full-container mt-2 text-center scroll-mt-header"
-      >
-        <div className="flex flex-wrap justify-center gap-3 mt-4 px-2 max-w-4xl mx-auto">
-          {facts.map((fact, i) => {
-            const Icon = fact.icon
-            return (
-              <div
-                key={i}
-                className="flex items-center gap-2 px-4 py-2 bg-gray-100 dark:bg-gray-800
-                                text-sm text-gray-700 dark:text-gray-300 rounded-full shadow-md transition"
-              >
-                <Icon className="text-blue-500 dark:text-blue-400 text-base" />
-                <span>{fact.label}</span>
-              </div>
-            )
-          })}
-        </div>
-
-        <p className="text-base sm:text-lg text-gray-600 dark:text-gray-300 max-w-4xl mx-auto text-left mb-6 mt-6">
-          A collaborative and detail-oriented Front-End Engineer with 3+ years of professional
-          experience, including 2 years in a dynamic e-commerce team at Wabow Information Inc.
-        </p>
-
-        <p className="text-base sm:text-lg text-gray-600 dark:text-gray-300 max-w-4xl mx-auto text-left">
-          Leverages a background in Industrial Design to build intuitive and engaging user
-          experiences with Typescript, Vue, and React. A quick learner adept at writing clean,
-          robust, and maintainable code to solve complex problems and deliver high-quality web
-          applications.
-        </p>
-
-        {/* Tech Stack */}
-        <div className="text-center mt-2">
-          <hr className="my-4 border-gray-500" />
-          <div className="flex flex-wrap justify-center gap-4 my-4">
-            {techStack.map(tech => {
-              const { icon: Icon, color, darkColor } = techStackMap[tech]
-              return (
-                <div
-                  key={tech}
-                  className="flex items-center gap-1 bg-gray-100 dark:bg-gray-800 px-2 py-1 rounded-sm"
-                  style={
-                    {
-                      ["--text-c"]: color,
-                      ["--text-c-dark"]: darkColor ?? color,
-                    } as TechIconColors
-                  }
-                >
-                  {Icon && (
-                    <Icon
-                      className="text-[var(--text-c)] dark:text-[var(--text-c-dark)]"
-                      size={20}
-                    />
-                  )}
-                  <span>{tech}</span>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </motion.div>
-
-      {/* Recent Work */}
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-        viewport={{ once: true }}
-        className="mt-16"
-      >
-        <ViewAllHeader title="Work Experience" pageUrl="/work" itemCount={work.length} />
-        <div className="grid gap-4">
-          {work.slice(0, 3).map((job, i) => (
-            <WorkItem key={i} {...job} />
-          ))}
         </div>
       </motion.div>
 
@@ -298,6 +203,82 @@ export default function Home() {
           </div>
         </motion.div>
       )}
+
+      {/* Brief Introduction */}
+      <motion.div
+        ref={BriefIntroRef}
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        className="main-container mt-2 text-center scroll-mt-header"
+      >
+        <h2 className="text-3xl font-extrabold text-primary text-shadow-[2px_2px_0px_rgba(0_0_0_/_0.6)] text-shadow-primary/50 leading-none dark:text-secondary dark:text-shadow-secondary/50">
+          A Bit About Me
+        </h2>
+
+        <div className="flex flex-col md:flex-row md:items-center md:gap-6">
+          <div className="flex-1">
+            <div className="flex flex-wrap justify-center gap-2 mt-4">
+              {facts.map((fact, index) => {
+                const { icon: Icon, label } = fact
+
+                return (
+                  <Tag key={index} color="secondary" className="flex items-center gap-1" outline>
+                    <Icon className="text-secondary" size={14} />
+                    <span>{label}</span>
+                  </Tag>
+                )
+              })}
+            </div>
+
+            <p className="mt-4 px-2 text-left text-dark dark:text-light">
+              Hi, I&apos;m Shan-Yu Chou, a Frontend Engineer with 2 years of software experience.
+              With a design background, I excel at creating intuitive user experiences using
+              Typescript, React, and Vue. I&apos;m also a quick learner adept at writing robust and
+              maintainable code to solve complex problems and deliver high-quality applications.
+            </p>
+
+            <p className="mt-4 px-2 text-left text-dark dark:text-light">
+              Currently seeking new opportunities as a React Front-End Engineer. Open to relocation
+              in America, Singapore, Germany, the Netherlands, Czech Republic, and Portugal, as well
+              as remote roles.
+            </p>
+          </div>
+
+          <Timeline className="flex-1 mt-6">
+            <TimelineItem
+              title="Color, Material, Finishing Designer"
+              subtitle="CLEVO Computer co."
+              duration="2021-07 - 2022-09"
+              location="Taipei, Taiwan"
+            />
+            <TimelineItem
+              title="Web Development Student"
+              subtitle="Alpha Camp"
+              duration="2022-10 - 2023-04"
+              location="Taipei, Taiwan"
+            />
+            <TimelineItem
+              title="Frontend Engineer"
+              subtitle="WACA Information Inc."
+              duration="2023-12 - Present"
+              location="Taipei, Taiwan"
+            />
+            <TimelineItem
+              title="Computer Science Student"
+              subtitle="Oregon State University"
+              duration="2024-09 - Present"
+              location="OR, USA (Remote)"
+            />
+          </Timeline>
+        </div>
+        <div className="flex justify-center">
+          <Link href="/about">
+            <Button className="mt-6">Learn More About Me</Button>
+          </Link>
+        </div>
+      </motion.div>
     </section>
   )
 }
